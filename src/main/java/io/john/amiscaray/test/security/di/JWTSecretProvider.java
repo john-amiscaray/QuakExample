cde28560a846dcf12db7214de25a0b6f805abff1
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-12-14 10:43:56 -0500

    Update JWT Secret to be taken from application properties

--- src/main/java/io/john/amiscaray/test/security/di/JWTSecretProvider.java
+++ src/main/java/io/john/amiscaray/test/security/di/JWTSecretProvider.java
@@ -3,13 +3,16 @@ package io.john.amiscaray.test.security.di;
 
 import io.john.amiscaray.quak.core.di.provider.annotation.Provide;
 import io.john.amiscaray.quak.core.di.provider.annotation.Provider;
+import io.john.amiscaray.quak.core.properties.ApplicationProperties;
+import io.john.amiscaray.quak.core.properties.ApplicationProperty;
 
 @Provider
 public class JWTSecretProvider {
 
     @Provide(dependencyName = "jwt")
     public String provideJWTSecret() {
-        return "Something secure";
+        var properties = ApplicationProperties.getInstance();
+        return properties.get(ApplicationProperty.JWT_SECRET_KEY);
     }
 
 }
